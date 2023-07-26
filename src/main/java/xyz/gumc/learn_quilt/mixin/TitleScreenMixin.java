f724fb07fd6f0525d1ada6cd2698e9b0cf7b33aa
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2023-07-26 04:03:11 +0000

    update

--- src/main/java/xyz/gumc/learn_quilt/mixin/TitleScreenMixin.java
+++ src/main/java/xyz/gumc/learn_quilt/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package xyz.gumc.learn_quilt.mixin;
 
-import com.example.example_mod.ExampleMod;
+import xyz.gumc.learn_quilt.LearnQuilt;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
